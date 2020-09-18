72bf1b2e60ba6eac177367023c16708ec4cc0fa0
Carlito Algarin <19lito96@gmail.com>
2020-09-18 17:20:24 -0400

    homework start up and I also added in row and column containers

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1.- Display current time using Moment.js
+    console.log(moment().format("dddd, MMMM Do"));
+//2.- Click event to save text area content to local storage
+
+//3.- When app opens, display content for each time block from local storage.
+    // - it can be done with a loop (more efficient) or by targeting each time block separately
+
+// 4.- Change time block background color depending on the current hour (past, present and future).
+console.log(moment().hour());
