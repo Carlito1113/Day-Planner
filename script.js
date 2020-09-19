44d3d278c6ba7b6bcf2e5c1d6ce6ae8862180318
Carlito Algarin <19lito96@gmail.com>
2020-09-18 21:17:36 -0400

    displayed moment js to the page

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+var currentDate = $("#currentDay");
+currentDate.text(moment().format("dddd, MMMM Do"));
+
 // 1.- Display current time using Moment.js
-    console.log(moment(time).format("dddd, MMMM Do"));
+   console.log(moment().format("dddd, MMMM Do"));
 //2.- Click event to save text area content to local storage
 
 //3.- When app opens, display content for each time block from local storage.
     // - it can be done with a loop (more efficient) or by targeting each time block separately
 
 // 4.- Change time block background color depending on the current hour (past, present and future).
-console.log(moment(time).hour());
+console.log(moment().hour());
