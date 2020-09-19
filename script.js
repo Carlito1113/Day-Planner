0849767d4f9a1bd13441ee05d4aec1cc4e10adca
Carlito Algarin <19lito96@gmail.com>
2020-09-18 21:25:57 -0400

    added time variables utilizing jquery

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 var currentDate = $("#currentDay");
 currentDate.text(moment().format("dddd, MMMM Do"));
 
+var nine = $("#nine");
+var ten  = $("ten");
+var eleven = $("eleven");
+var twelve = $("twelve");
+var one =$("one");
+var two =$("two");
+var three = $("three");
+var four = $("four");
+var five = $("five");
+
+
+
+
+
+
+
+
+
+
+
 // 1.- Display current time using Moment.js
    console.log(moment().format("dddd, MMMM Do"));
 //2.- Click event to save text area content to local storage
